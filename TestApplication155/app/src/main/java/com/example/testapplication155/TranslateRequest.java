d1db45c5428c529aea52e48726655f326cd58d88
Van Tuinen, Jason <vantuinen1@llnl.gov>
2022-04-25 22:14:26 -0700

    Adding translate functionality to new project

--- TestApplication155/app/src/main/java/com/example/testapplication155/TranslateRequest.java
+++ TestApplication155/app/src/main/java/com/example/testapplication155/TranslateRequest.java
@@ -1,2 +1,27 @@
-package com.example.testapplication155;public class TranslateRequest {
+package com.example.testapplication155;
+
+public class TranslateRequest {
+    String[] q;
+    String target;
+
+    public TranslateRequest(String[] q, String target) {
+        this.q = q;
+        this.target = target;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public String[] getQ() {
+        return q;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public void setQ(String[] q) {
+        this.q = q;
+    }
 }
